a3a6c1e138066fe0fbf7d1ec77a87a58b0a3b1b2
Imil <Mil2163444@mail.ru>
2023-03-08 09:37:54 +0300

    Найти сумму чисел от 1 до А

---
+++ Lessons/Program.cs
@@ -0,0 +1,15 @@
+﻿//Найти сумму чисел от 1 до А
+Console.WriteLine("Введите число А: ");
+int number = int.Parse(Console.ReadLine());
+int firstElement = 1;
+int sum = 0;
+
+for (int i = 0; i < number; i++)
+{
+    sum = sum + firstElement;
+    firstElement += 1;
+}
+Console.WriteLine(sum);
+
+Console.ReadLine();
+

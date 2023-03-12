cb29e048a213296156aa785ec9fa0d8520737a13
Imil <Mil2163444@mail.ru>
2023-03-12 22:48:11 +0300

    29. Написать программу вычисления произведения чисел от 1 до N

---
+++ Lessons_29i/Program.cs
@@ -0,0 +1,10 @@
+﻿//29. Написать программу вычисления произведения чисел от 1 до N
+System.Console.WriteLine("Задайте число N: ");
+int number = int.Parse(Console.ReadLine());
+int result = 1;
+
+for (int i = 2; i < number +1; i++)
+{
+    result = result * i;
+}
+System.Console.WriteLine(result);

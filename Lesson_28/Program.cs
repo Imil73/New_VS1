83940f6ad48bbce57859a04579a1bff91ad81f34
Imil <Mil2163444@mail.ru>
2023-03-12 22:29:14 +0300

    28. Подсчитать сумму цифр в числе

---
+++ Lesson_28/Program.cs
@@ -0,0 +1,12 @@
+﻿//28. Подсчитать сумму цифр в числе
+System.Console.WriteLine("Задайте число: ");
+int number = int.Parse(Console.ReadLine());
+int i = 0;
+
+while(number != 0)
+{    
+     i += number %10;
+     number = number / 10;
+}
+System.Console.WriteLine(i);
+

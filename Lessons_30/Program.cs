3f1f87d5f28f0d0fee54b4ba4db8dcd343d6d2e7
Imil <Mil2163444@mail.ru>
2023-03-12 23:01:58 +0300

    30. Показать кубы чисел, заканчивающихся на четную цифру

---
+++ Lessons_30/Program.cs
@@ -0,0 +1,14 @@
+﻿//30. Показать кубы чисел, заканчивающихся на четную цифру
+System.Console.WriteLine("Задайте число N: ");
+int number = int.Parse(Console.ReadLine());
+int result =1;
+ 
+result = number % 2;
+if( result ==0 )
+{
+    System.Console.WriteLine($"{number * number * number}");
+}
+else
+{
+    System.Console.WriteLine("Число не подходит");
+}

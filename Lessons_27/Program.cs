fa696ab81d9e28eb7c3b92ded26452f4840688de
Imil <Mil2163444@mail.ru>
2023-03-12 21:35:51 +0300

    27. Определить количество цифр в числе;

---
+++ Lessons_27/Program.cs
@@ -0,0 +1,17 @@
+﻿//27. Определить количество цифр в числе;
+System.Console.WriteLine("Задайте число: ");
+int number = int.Parse(Console.ReadLine());
+int i = 0;
+
+while(number != 0)
+{
+     number = number / 10;
+     i += 1;
+}
+System.Console.WriteLine(i);
+
+
+
+
+
+

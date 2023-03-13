d80f15382e527e71b04abcabcf62fd2754c131b9
Imil <Mil2163444@mail.ru>
2023-03-13 13:44:16 +0300

    31. Задать массив из 8 элементов и вывести их на экран

---
+++ Lessons_31/Program.cs
@@ -0,0 +1,9 @@
+﻿//31. Задать массив из 8 элементов и вывести их на экран
+
+int[] array = {1,2,3,4,5,6,7,8};
+
+for (int i = 0; i < array.Length; i++)
+{
+   System.Console.Write($"{array[i]} "); 
+}
+

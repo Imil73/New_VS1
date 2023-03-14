e5bb6be8098a184aecc48da0b4a931ed8c4563c2
Imil <Mil2163444@mail.ru>
2023-03-15 00:27:03 +0300

    0. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом

---
+++ Lessons_39/Program.cs
@@ -0,0 +1,24 @@
+﻿//40. В Указанном массиве вещественных чисел найдите разницу 
+//между максимальным и минимальным элементом
+
+int[] array = new int[5];
+Random random = new Random();
+
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0,20);
+    System.Console.WriteLine(array[i]);
+}
+
+int max = array[0];
+int min = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+   if(array[i] > max) max = array[i];
+   if(array[i] < min)
+    min = array[i];
+}
+System.Console.WriteLine($"Разница: {max - min}");
+
